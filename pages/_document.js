737469486626e3d0f57e9e2c3e45b82bddea7fa5
moonwolf <liaomk@gmail.com>
2024-09-29 17:14:09 +0800

    add tongji

--- pages/_document.js
+++ pages/_document.js
@@ -2,9 +2,15 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
-      <body className="antialiased">
+    <Html>
+      <Head>
+        <script 
+          defer 
+          data-domain="cryptoadvisorai.xyz" 
+          src="https://app.pageview.app/js/script.js"
+        />
+      </Head>
+      <body>
         <Main />
         <NextScript />
       </body>
